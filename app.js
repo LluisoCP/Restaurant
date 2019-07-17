45b46ed950cbdf208848a9f34769387ff9a00b18
Lluis <lluiso.aloha@gmail.com>
2019-07-18 00:25:49 +0200

    Express REST API server with Express, Mongo and Mongoose Part 1

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const dishRouter = require('./routes/dishRouter');
 const leaderRouter = require('./routes/leaderRouter');
 const promoRouter = require('./routes/promoRouter');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27018/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+
+}, (err) => { console.log(err); });
+
 
 var app = express();
 
