e69ae16b033fdbb9b5c7c816d576bf2c4a265c51
Lluis <lluiso.aloha@gmail.com>
2019-08-06 19:00:57 +0200

    Express Sessions Part 2

--- app.js
+++ app.js
@@ -45,43 +45,41 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   // console.log(req.headers);
   console.log(req.session);
 
   if (!req.session.user) {
-    let authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      let err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-
-    let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    let err = new Error('You are not authenticated.')
+    err.status = 401;
+    return next(err);
+  /*
+    // let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     
-    let username = auth[0];
-    let password = auth[1];
+    // let username = auth[0];
+    // let password = auth[1];
 
-    if (username === 'admin' && password === 'password'){
-      // res.cookie('user', 'admin', { signed: true });
-      req.session.user = 'admin';
-      next();
-    } else {
-      let err = new Error('You are not properly authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    // if (username === 'admin' && password === 'password'){
+    //   // res.cookie('user', 'admin', { signed: true });
+    //   req.session.user = 'admin';
+    //   next();
+    // } else {
+    //   let err = new Error('You are not properly authenticated!');
+    //   res.setHeader('WWW-Authenticate', 'Basic');
+    //   err.status = 401;
+    //   return next(err);
+    // }
+  */
   } else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next();
     } else {
 
       let err = new Error('You are not authenticated!');
-
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -92,8 +90,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
